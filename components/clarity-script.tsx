a3e0e0db0c01f90516bb630510bdb001b8116d3e
Hin <tonghohin77@gmail.com>
2025-08-27 23:48:17 -0400

    feat: clarity

--- components/clarity-script.tsx
+++ components/clarity-script.tsx
@@ -1,19 +1,18 @@
-import Script from "next/script";
+"use client";
 
-export function Clarity() {
+import Clarity from "@microsoft/clarity";
+import { useEffect } from "react";
+
+export function ClarityScript() {
     const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID;
-
     if (!clarityProjectId) return null;
 
-    return (
-        <Script id="microsoft-clarity" type="text/javascript" strategy="beforeInteractive">
-            {`
-                (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "${clarityProjectId}");
-            `}
-        </Script>
-    );
+    useEffect(() => {
+        if (window.location.hostname === "localhost") return;
+
+        Clarity.init(clarityProjectId);
+        Clarity.consent();
+    }, [clarityProjectId]);
+
+    return null;
 }
